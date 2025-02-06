25b051acf744c24c1258ef10db87ee85924294f7
RinnesyOhara <771278979@qq.com>
2025-02-06 22:55:43 +0800

    rust 2025.2.6

--- src/operators.rs
+++ src/operators.rs
@@ -77,13 +77,18 @@ pub fn rms_norm(y: &mut Tensor<f32>, x: &Tensor<f32>, w: &Tensor<f32>, epsilon: 
 // y = silu(x) * y
 // hint: this is an element-wise operation
 pub fn swiglu(y: &mut Tensor<f32>, x: &Tensor<f32>) {
-    // let len = y.size();
-    // assert!(len == x.size());
+    let len = y.size();
+    assert!(len == x.size(),"Input and output vectors must have the same length.");
 
-    // let _y = unsafe { y.data_mut() };
-    // let _x = x.data();
+    let _y = unsafe { y.data_mut() };
+    let _x = x.data();
 
-    todo!("实现 silu，这里给了一些前期准备工作的提示，你可以参考")
+    for i in 0..len {
+        let sigmoid_x = 1.0/(1.0+(-_x[i]).exp());
+        let sliu_x = sigmoid_x*_x[i];
+        _y[i] = sliu_x*_y[i];
+    }
+    
 }
 
 // C = beta * C + alpha * A @ B^T
